22d61a38f22adf5a35c98081bec7ad65fa11ead6
Tony Leng <lengtk@gmail.com>
2022-10-05 14:43:28 +0900

    fixed single selection of ratings

--- script.js
+++ script.js
@@ -1,19 +1,15 @@
 const ratings = document.querySelectorAll('.rate-num');
 
-// const toggleRating = () => {
-//   ratings.forEach(rating => {
-//     rating.currentTarget.classList.toggle('active');
-//     console.log('rating')
-//   });
-// }
-
-// ratings.forEach(rating => ('click', () => {
-//   rating.currentTarget.classList.toggle('active');
-//   console.log('rating')
-// }));
-
-
 for (let rating of ratings) {
 rating.addEventListener('click', () => {
+  for (let rating of ratings) {
+    rating.classList.remove('active');
+  }
   rating.classList.toggle('active');
+  if (rating.classList.contains('active')) {
+    rating.classList.remove('disabled')
+  }
+  else {
+    rating.classList.add('disabled');
+  }
 })}
