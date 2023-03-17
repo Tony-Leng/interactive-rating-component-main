53a7d16d2efbbb0cf3e5fa1ac3e5a2079c5503df
Tony Leng <lengtk@gmail.com>
2023-03-17 23:49:40 +0900

    made selected rating appear on thank you

--- script.js
+++ script.js
@@ -1,21 +1,25 @@
 const submitbtn = document.querySelector('#submit');
 const thankyoucard = document.querySelector('.thankyou-body');
 const ratingcard = document.querySelector('.rating-body');
-const ratingnum = document.querySelector('.rating-nums');
+const ratingnum = document.querySelectorAll('.rate-num');
+const disabled = document.querySelector('.disabled');
+const result = document.querySelector('#final-rating');
 
 
-// ratingnum.forEach(rating => {
-//   rating.addEventListener('click', () => {
-//     rating.addEventListener('click', () => {
-//       rating.classList.add('disable');
-//       rating.classList.remove('active')
-//     } )
-//     rating.classList.add('active');
-//     rating.classList.remove('disabled')
-//   })
-// });
+ratingnum.forEach(rating => {
+  rating.addEventListener('click', () => {
+    ratingnum.forEach((ifActive) => {
+      ifActive.classList.remove('active');
+    })
+    rating.classList.add('active');
+    disabled.classList.remove('disabled');
+  })
+});
 
 submitbtn.addEventListener('click', () => {
+  const isActive = document.querySelector('.active');
+  let ratingValue = isActive.textContent;
   thankyoucard.classList.remove('hidden');
   ratingcard.classList.add('hidden');
+  result.textContent = ratingValue;
 })
